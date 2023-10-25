3d84feaf9e79ff8805c3d790c166a95503faa3a8
Jesus <jesusvazquez2477@gmail.com>
2023-10-25 11:26:28 +0200

    Declaro variable cuerpoSnake con un array vacio, implemento bucle for [200~ for (let i = cuerpoSnake.length - 1; i > 0; i--) { y utilizo el metodo push para agregar elementos al final del array, implemento bucle for con condicional if para garantizar que solo el primer elemento de cuerpoSnake se muestre como la cabeza de la serpiente, y los elementos posteriores se muestren como el cuerpo

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ let snakeX = 5,
   snakeY = 10;
 let velocidadX = 0,
   velocidadY = 0;
+let cuerpoSnake = [];
 
 const cambiarPosicionComida = () => {
   comidaX = Math.floor(Math.random() * 30) + 1;
@@ -32,20 +33,29 @@ const cambioDireccion = (e) => {
 
 const initGame = () => {
   let htmlMarkup = `<div class="comida" style="grid-area: ${comidaY} / ${comidaX}"></div>`;
+  for (let i = cuerpoSnake.length - 1; i > 0; i--) {
+    cuerpoSnake[i] = cuerpoSnake[i - 1];
+  }
   // Comprueba si la cabeza de la serpiente coincide con la comida antes de actualizar el cuerpo.
   if (snakeX === comidaX && snakeY === comidaY) {
     cambiarPosicionComida();
+    cuerpoSnake.push([comidaX, comidaY]);
   }
+  cuerpoSnake[0] = [snakeX, snakeY];
   snakeX += velocidadX;
   snakeY += velocidadY; // y = 10 - 1 = 9
-
+  for (let i = 0; i < cuerpoSnake.length; i++) {
+    if (i === 0) {
+      htmlMarkup += `<div class="cabeza" style="grid-area: ${cuerpoSnake[i][1]} / ${cuerpoSnake[i][0]}"></div>`;
+    } else {
+      htmlMarkup += `<div class="cuerpoSerpiente" style="grid-area: ${cuerpoSnake[i][1]} / ${cuerpoSnake[i][0]}"></div>`;
+    }
+  }
   // Utilizo += para agregar la serpiente sin sobreescribir la comida
   htmlMarkup += `<div class="cuerpoSerpiente" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
-
   tablero.innerHTML = htmlMarkup;
 };
 
 cambiarPosicionComida();
 setIntervalId = setInterval(initGame, 125);
 document.addEventListener("keydown", cambioDireccion);
-initGame();
