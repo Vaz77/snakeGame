bb276353565bb1d18a92194a2964639bc5be624c
Jesus <jesusvazquez2477@gmail.com>
2023-10-25 10:21:59 +0200

    Implemento coordenadas de la serpiente y aplico estilos para mostrarlo, vuelvo a utilizar htmlMarkup pero esta vez con += para no sobreescribir la comida al mostrar la serpiente

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ const tablero = document.querySelector(".tablero");
 
 let comidaX = 13,
   comidaY = 10;
+let snakeX = 5,
+  snakeY = 10;
 
 const cambiarPosicionComida = () => {
   comidaX = Math.floor(Math.random() * 30) + 1;
@@ -11,6 +13,9 @@ const cambiarPosicionComida = () => {
 
 const initGame = () => {
   let htmlMarkup = `<div class="comida" style="grid-area: ${comidaY} / ${comidaX}"></div>`;
+  // Utilizo += para agregar la serpiente sin sobreescribir la comida
+  htmlMarkup += `<div class="cuerpoSerpiente" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+
   tablero.innerHTML = htmlMarkup;
 };
 
