dea4f4ac84f08fb4d25bbbfe3f7b5dc293609788
Jesus <jesusvazquez2477@gmail.com>
2023-10-25 10:45:05 +0200

    Implemento funcion cambioDireccion, si se presionan las teclas de flecha, la función actualiza las variables velocidadX y velocidadY en función de la tecla presionada. Por ejemplo, si se presiona ArrowUp y la serpiente no se está moviendo hacia abajo (velocidadY != 1), la velocidadY se establece en -1, lo que hace que la serpiente se mueva hacia arriba, tambien utilizo setInterval para llamar repetidamente a la función initGame cada 125 milisegundos. Esto actualiza continuamente la posición de la serpiente en el tablero, lo que simula el movimiento de la serpiente.

--- game.js
+++ game.js
@@ -4,15 +4,36 @@ let comidaX = 13,
   comidaY = 10;
 let snakeX = 5,
   snakeY = 10;
+let velocidadX = 0,
+  velocidadY = 0;
 
 const cambiarPosicionComida = () => {
   comidaX = Math.floor(Math.random() * 30) + 1;
   comidaY = Math.floor(Math.random() * 30) + 1;
-  console.log(comidaX);
+};
+
+const cambioDireccion = (e) => {
+  console.log(e);
+  if (e.key === "ArrowUp" && velocidadY != 1) {
+    velocidadX = 0;
+    velocidadY = -1;
+  } else if (e.key === "ArrowDown" && velocidadY != -1) {
+    velocidadX = 0;
+    velocidadY = 1;
+  } else if (e.key === "ArrowLeft" && velocidadX != 1) {
+    velocidadX = -1;
+    velocidadY = 0;
+  } else if (e.key === "ArrowRight" && velocidadX != -1) {
+    velocidadX = 1;
+    velocidadY = 0;
+  }
+  initGame();
 };
 
 const initGame = () => {
   let htmlMarkup = `<div class="comida" style="grid-area: ${comidaY} / ${comidaX}"></div>`;
+  snakeX += velocidadX;
+  snakeY += velocidadY; // y = 10 - 1 = 9
   // Utilizo += para agregar la serpiente sin sobreescribir la comida
   htmlMarkup += `<div class="cuerpoSerpiente" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
 
@@ -20,4 +41,6 @@ const initGame = () => {
 };
 
 cambiarPosicionComida();
+setIntervalId = setInterval(initGame, 125);
+document.addEventListener("keydown", cambioDireccion);
 initGame();
